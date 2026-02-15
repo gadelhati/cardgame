52a84c5693775e3977b675324ff3955e6fe3619b
gadelhati <gadelha.ti@gmail.com>
2026-02-15 08:00:14 -0300

    fix: overflow absolute image card

--- src/Card.tsx
+++ src/Card.tsx
@@ -3,20 +3,18 @@ import image from './assets/knight.png'
 
 export const Card = () => {
     return (
-        // <article className='background'>
-            <div className='card'>
-                <header>
-                    <div>Type</div>
-                    <div>Title</div>
-                    <div>Cost</div>
-                </header>
-                <img src={image} alt='Card image' />
-                <div className='text'>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit. Neque porro quisquam est q...</div>
-                <footer>
-                    <div>ATK</div>
-                    <div>DEF</div>
-                </footer>
-            </div>
-        // </article>
+        <article className='card'>
+            <header>
+                <div>Type</div>
+                <div>Title</div>
+                <div>Cost</div>
+            </header>
+            <img src={image} alt='Card image' />
+            <div className='text'>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit. Neque porro quisquam est q...</div>
+            <footer>
+                <div>ATK</div>
+                <div>DEF</div>
+            </footer>
+        </article>
     )
 }
